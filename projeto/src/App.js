ea9f35d51363cec84a60612cc61605f66ed8f39a
Marcos Vinicius <bastosmarcos90@gmail.com>
2025-03-09 19:21:20 -0300

    commit

--- projeto/src/App.js
+++ projeto/src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { MyNavbar } from './components/navbar';
 
 function App() {
   return (
     <div className="App">
-      
+      <MyNavbar />
     </div>
   );
 }
--- projeto/src/components/navbar.js
+++ projeto/src/components/navbar.js
@@ -2,55 +2,58 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { useEffect, useState } from 'react';
-export const navbar = () => {
-  const [activeLink, setActivateLink] = useState('home');
-  const [scrolled, seScrolled] = useState(False);
+import logo from '../assets/img/logo.svg';
+import navIcon1 from '../assets/img/nav-icon1.svg';
+import navIcon2 from '../assets/img/nav-icon2.svg';
+import navIcon3 from '../assets/img/nav-icon3.svg';
 
+export const MyNavbar = () => {
+  const [activeLink, setActivateLink] = useState('home');
+  const [scrolled, setScrolled] = useState(false);
   useEffect (()=>{
     const onScroll = () => {
-      if (widow.scrollY > 50 ) {
-        seScrolled(true);
+      if (window.scrollY > 50 ) {
+        setScrolled(true);
       } else {
-        seScrolled(false);
+        setScrolled(false);
       }
     }
-    
-    widow.addEventListener("scroll", onScroll);
-
-    return () => widow.removeEventListener ("scroll", onScroll);
+    window.addEventListener("scroll", onScroll);
+    return () => window.removeEventListener ("scroll", onScroll);
   }, [])
-    return (
-        function Navbar() {
+    const onUpdateActiveLink = (value) => {
+      setActivateLink(value);
+    }
+
+    
             return (
               <Navbar expand="lg" className={scrolled ? "scrolled" : ""}>
                 <Container>
                   <Navbar.Brand href="#home">
-                    <img src={''} alt="logo"></img>
+                    <img src={logo} alt="logo"></img>
                   </Navbar.Brand>
                   <Navbar.Toggle aria-controls="basic-navbar-nav"> 
                     <span className='navbar-toggler-icon'></span> 
                   </Navbar.Toggle>
                   <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
-                      <Nav.Link href="#home">Home</Nav.Link>
-                      <Nav.Link href="skills">skills</Nav.Link>
-                      <Nav.Link href="Projetos">Projetos</Nav.Link>
+                      <Nav.Link href="#home" className={activeLink === 'home' ? 'active navbar-link':'navbar-link'} onClick={() => onUpdateActiveLink('home')}>Home</Nav.Link>
+                      <Nav.Link href="#skills" className={activeLink === 'skills' ? 'active navbar-link':'navbar-link'} onClick={() => onUpdateActiveLink('skills')}>skills</Nav.Link>
+                      <Nav.Link href="#Projetos" className={activeLink === 'projetos' ? 'active navbar-link':'navbar-link'} onClick={() => onUpdateActiveLink('projetos')}>Projetos</Nav.Link>
                     </Nav>
                     <span className='navbar-text'>
                       <div className='social-icon'>
-                        <a href='#'><img src="imagem.jpg" alt="descrição da img"/></a>
-                        <a href='#'><img src="imagem.jpg" alt="descrição da img"/></a>
-                        <a href='#'><img src="imagem.jpg" alt="descrição da img"/></a>
+                        <a href='#'><img src={navIcon1} alt="descrição da img"/></a>
+                        <a href='#'><img src={navIcon2} alt="descrição da img"/></a>
+                        <a href='#'><img src={navIcon3} alt="descrição da img"/></a>
                       </div>
-                      <butoon className="botão" onClick={() => console.log ("connect")}><span>
-                        Vamos conectar</span></butoon>
+                      <button className="botão" onClick={() => console.log ("connect")}><span>
+                        Vamos conectar</span></button>
                     </span>
                   </Navbar.Collapse>
                 </Container>
               </Navbar>
             );
           }
-    )
-}
 
-export default BasicExample;
+export default MyNavbar;
